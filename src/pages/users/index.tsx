c5ca79a1e64442cfefed2ff3ca9359e4b92abf62
Vinicius Martins <vinicius.victor.sm@gmail.com>
2021-08-25 15:32:02 -0300

    refactoring users query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -17,38 +17,15 @@ import {
 } from '@chakra-ui/react';
 import Link from 'next/link';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
-import { useQuery } from 'react-query';
-import { User } from '../../models/User';
 
 //components
 import { Header } from '../../components/Header';
 import Pagination from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
+import { useUsers } from '../../services/hooks/useUsers';
 
 export default function UserList() {
-  const {
-    data: usersList,
-    isLoading,
-    error,
-  } = useQuery<User[]>('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users');
-    const data = await response.json();
-
-    const users = data.users.map((user) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric',
-        }),
-      };
-    });
-
-    return users;
-  });
+  const { data: usersList, isLoading, isFetching, error } = useUsers();
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -66,6 +43,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {isFetching && !isLoading && (
+                <Spinner color="gray.500" ml="4" size="sm" />
+              )}
             </Heading>
 
             <Link href="/users/create" passHref>
