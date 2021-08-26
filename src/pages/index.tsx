9f2ecded7319b6a0a38c2873e0c3b456d45873a6
Vinicius Martins <vinicius.victor.sm@gmail.com>
2021-08-26 13:40:09 -0300

    signIn method from auth hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 
 import { Input } from '../components/Form/Input';
+import { useAuth } from '../auth/hooks/useAuth';
 
 type SignInFormData = {
   email: string;
@@ -16,14 +17,14 @@ const signInFormSchema = Yup.object().shape({
 });
 
 export default function Home() {
+  const { signIn, isAuthenticated } = useAuth();
+  
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(signInFormSchema),
   });
 
   const handleSignIn: SubmitHandler<SignInFormData> = async (data) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-
-    console.log(data);
+    await signIn({ email: data.email, password: data.password });
   };
 
   const { errors } = formState;
