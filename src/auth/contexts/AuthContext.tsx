40750225de41303ce4ff242329f198998c6f372c
Vinicius Martins <vinicius.victor.sm@gmail.com>
2021-08-26 13:39:01 -0300

    auth context

---
+++ src/auth/contexts/AuthContext.tsx
@@ -0,0 +1,6 @@
+import { createContext } from 'react';
+import { AuthContextData } from '../types/AuthContextData';
+
+export const AuthContext = createContext<AuthContextData>(
+  {} as AuthContextData
+);
