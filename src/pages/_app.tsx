6d072156cc897d5efff13c23636295e06ab05c4d
Vinicius Martins <vinicius.victor.sm@gmail.com>
2021-08-26 10:22:17 -0300

    data prefetching

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,12 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../styles/theme';
 import { SidebarDrawerProvider } from '../context/SidebarDrawerContext';
 import { makeServer } from '../services/mirage';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClientProvider } from 'react-query';
+import { queryClient } from '../services/query/queryClient';
 
 if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
-
-const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
