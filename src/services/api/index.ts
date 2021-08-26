cc0554247330eb3d6477039a0212a99db1dc3139
Vinicius Martins <vinicius.victor.sm@gmail.com>
2021-08-26 14:16:41 -0300

    authApi service

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,5 +1,11 @@
 import axios from 'axios';
 
+//miragejs
 export const api = axios.create({
   baseURL: 'http://localhost:3000/api',
+});
+
+//nodejs api
+export const authApi = axios.create({
+  baseURL: 'http://localhost:3333',
 });
